a58909e906711e96fc17c03582aebbb0995702c9
Yasin Shamrat <yshamrat@gmail.com>
2020-02-10 01:48:57 +0600

    screen shot uploaded

--- iOS Neu Morphic/ViewController.swift
+++ iOS Neu Morphic/ViewController.swift
@@ -19,18 +19,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //MARK:- For Setting Shadow on left and top side of the view
+        
         view1.layer.cornerRadius = 20
-//        view1.layer.shadowColor = UIColor(named: "lightShadeOfBgColor")?.cgColor
-view1.layer.shadowColor = UIColor.white.cgColor
+        view1.layer.shadowColor = UIColor.white.cgColor
         view1.layer.shadowOpacity = 0.3
         view1.layer.shadowOffset = CGSize(width: -3, height: -3)
         view1.layer.shadowRadius = 2
         
         
         //MARK:- For Setting Shadow on left and top side of the view
+        
         view2.layer.cornerRadius = 20
-//        view2.layer.shadowColor = UIColor(named: "lightShadeOfBgColor")?.cgColor
-view2.layer.shadowColor = UIColor.black.cgColor
+        view2.layer.shadowColor = UIColor.black.cgColor
         view2.layer.shadowOpacity = 0.5
         view2.layer.shadowOffset = CGSize(width: 3, height: 3)
         view2.layer.shadowRadius = 2
